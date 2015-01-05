5e0853d5d3dd72062fe9b609c44ee500cd4a5b0d
Basil <n@adar.ch>
2015-01-05 23:22:07 +0100

    added test for multiple tracking responses

--- tests/AspsmsSuccessTest.php
+++ tests/AspsmsSuccessTest.php
@@ -46,14 +46,17 @@ class AspsmsSuccessTest extends \PHPUnit_Framework_TestCase
 
     public function testDeliveryStatus()
     {
-        sleep(5); // wating for aspsms to proceed the delivery
-        $response = $this->aspsms->deliveryStatus(SMS_TRACKING);
-        /* test does only work for multiple response codes */
-        /*
-        $this->assertArrayHasKey(SMS_TRACKING, $response);
-        $array = $response[SMS_TRACKING];
-        */
-        $array = $response;
+        // wating for aspsms to proceed the delivery
+        sleep(5);
+        
+        // get the response
+        $array = $this->aspsms->deliveryStatus(SMS_TRACKING);
+        
+        // see if multiple tracking codes exist.
+        if (array_key_exists(SMS_TRACKING, $response)) {
+            $array = $response[SMS_TRACKING];
+        }
+        
         $this->assertArrayHasKey('transactionReferenceNumber', $array);
         $this->assertArrayHasKey('deliveryStatus', $array);
         $this->assertArrayHasKey('transactionReferenceNumber', $array);
