dc459411b7349629e7e1054ac8c631cfebf63064
Nadar <git@nadar.io>
2016-04-15 22:49:05 +0200

    version 1.0.1 release bump

--- src/Aspsms.php
+++ src/Aspsms.php
@@ -16,7 +16,7 @@ class Aspsms
     /**
      * Contains the Aspms Class Version
      */
-    const VERSION = 2.0;
+    const VERSION = '1.0.1';
 
     /**
      * Contains the services url [status 30.01.2013]
